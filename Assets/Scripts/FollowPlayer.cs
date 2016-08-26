5918137abd8c66255462367a6d4054b92ff82542
Makoto Ishihara <los_tau_ern330@yahoo.co.jp>
2016-08-26 12:30:34 +0900

    First Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;  //ターゲットへの参照
+    public Vector3 offset;   //相対座標
+
+    void Start() {
+        offset = GetComponent<Transform>().position - target.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 自分自身の座標に、targetの座標に相対座標を足した値を設定する
+        GetComponent<Transform>().position = target.position + offset;
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10; 
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //入力をxとzに代入
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+}
